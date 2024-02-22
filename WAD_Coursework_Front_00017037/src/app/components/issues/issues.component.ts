3dc1139d74aad272ca5e87f16e2819034db83013
00017037 <wiut00017037@gmail.com>
2024-02-22 22:44:03 +0500

    refactor: remove comments

--- WAD_Coursework_Front_00017037/src/app/components/issues/issues.component.ts
+++ WAD_Coursework_Front_00017037/src/app/components/issues/issues.component.ts
@@ -1,21 +1,51 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { IIssue, Severity } from '../../interfaces/issue.interface';
 import { SeverityDirective } from '../../directives/serverity.directive';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
 import { IssuesClient } from '../../services/issues-client.service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, switchMap, take, tap } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { IDialogData } from '../../interfaces/dialog-data.interface';
+import { IssueFormComponent } from '../issue-form/issue-form.component';
 
 @Component({
   selector: 'app-issues',
   standalone: true,
-  imports: [CommonModule, SeverityDirective, MatButtonModule, RouterModule],
+  imports: [
+    CommonModule,
+    SeverityDirective,
+    MatButtonModule,
+    RouterModule,
+    MatIconModule,
+  ],
   templateUrl: './issues.component.html',
   styleUrl: './issues.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IssuesComponent {
-  issues$: Observable<IIssue[]> = this.issuesClient.getAllIssues();
+  updateIssuesAction$ = new BehaviorSubject(true);
 
-  constructor(private issuesClient: IssuesClient) {}
+  issues$: Observable<IIssue[]> = this.updateIssuesAction$.pipe(
+    switchMap(() => this.issuesClient.getAllIssues())
+  );
+
+  constructor(private issuesClient: IssuesClient, private dialog: MatDialog) {}
+
+  addIssue() {
+    const data: IDialogData = {
+      isEditMode: false,
+    };
+
+    const dialogRef = this.dialog.open(IssueFormComponent, { data });
+    dialogRef
+      .afterClosed()
+      .pipe(
+        tap(() => this.updateIssuesAction$.next(true)),
+        take(1)
+      )
+      .subscribe();
+  }
 }
--- WAD_Coursework_Front_00017037/src/app/interceptors/base-url-interceptor.ts
+++ WAD_Coursework_Front_00017037/src/app/interceptors/base-url-interceptor.ts
@@ -1,16 +1,11 @@
-import { HttpInterceptorFn } from "@angular/common/http";
+import { HttpInterceptorFn } from '@angular/common/http';
 
-
-// Define the base URL
 const baseUrl = 'http://localhost:5187/api';
 
-export const baseUrlInterceptor:HttpInterceptorFn = (request, next) => {
-  // Check if the URL is already absolute
+export const baseUrlInterceptor: HttpInterceptorFn = (request, next) => {
   if (!request.url.startsWith('http')) {
-    // Prepend the base URL to the request URL if it's not absolute
     const apiReq = request.clone({ url: `${baseUrl}/${request.url}` });
     return next(apiReq);
   }
-  // For absolute URLs, proceed without modification
   return next(request);
-}
+};
